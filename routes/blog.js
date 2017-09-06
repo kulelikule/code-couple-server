2f4b7316a68baa00cf83aa92da439d7369bbd70d
kulelikule <821039485@qq.com>
2017-09-06 22:44:08 +0800

    [MOD] 完成对文章的查询和分页功能

--- routes/blog.js
+++ routes/blog.js
@@ -2,13 +2,20 @@ var express = require('express');
 var router = express.Router();
 var Article = require('../db/schema/article');
 
-/* GET home page. */
-router.get('/getNewArticles', function(req, res) {
-    Article.find({}, ['_id', 'title', 'date']).limit(10).sort({'date': -1}).exec().then(function(result) {
+router.get('/getArticles', function(req, res){
+    var pageNo = Math.max(1, req.query.pageNo) - 1
+    var limit = Number(req.query.limit) || 20
+    var countPromise = Article.count().exec()
+    var artclesPromise = Article.find({}, ['_id', 'title', 'date']).skip(pageNo * limit).limit(limit).sort({'date': -1}).exec()
+
+    Promise.all([countPromise, artclesPromise]).then(function(result) {
         res.send({
             success: true,
             message: '获取最新文章列表成功',
-            result: result
+            result: {
+                count: result[0],
+                data: result[1]
+            }
         });
     }).catch(function(e){
         console.log(e);
@@ -18,7 +25,7 @@ router.get('/getNewArticles', function(req, res) {
             result: []
         });
     });
-});
+})
 
 router.get('/getArticleDetails', function (req, res) {
     Article.findById(req.query.id).exec().then(function(result){
